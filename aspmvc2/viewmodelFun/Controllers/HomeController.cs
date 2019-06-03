f62f07570ab52e794d093ede0aa48e3034ff9095
Brett Jory <bjory7@gmail.com>
2019-06-03 12:34:38 -0700

    finished assignment viewmodel fun

---
+++ aspmvc2/viewmodelFun/Controllers/HomeController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using viewmodelFun.Models;
+
+namespace viewmodelFun.Controllers
+{
+    public class HomeController : Controller
+    {
+        [Route("")]
+        [HttpGet]
+        public IActionResult Index()
+        {
+					string message = "MVC in action";
+          return View("Index", message);
+        }
+
+				[HttpGet("numbers")]
+				public IActionResult Numbers()
+				{
+					int[] nums = {0,3,5,12,43};
+					return View("Numbers", nums);
+				}
+
+				[HttpGet("users")]
+				public IActionResult Users()
+				{
+					User[] users = {
+						new User("Chika", "Fujiwara"),
+						new User("Mai", "Sakurajima"),
+						new User("Kaguya", "Shinomiya"),
+						new User("Madoka", "Akame")
+					};
+					return View(users);
+				}
+
+				[HttpGet("user")]
+				public IActionResult User()
+				{
+					User user = new User("Kei", "Shirogane");
+					return View(user);
+				}
+    }
+}
